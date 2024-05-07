63d8bbb75c950df8c1bec75a37aa1a7d6d3034a7
NoahCodes12 <161295411+NoahCodes12@users.noreply.github.com>
2024-05-07 11:20:15 +0200

    Incredibly small change
    only very minor changes

--- MainCode.js
+++ MainCode.js
@@ -1,3 +1,5 @@
 
-document.getElementById("Game")
-getContext(2d);
+const canvas = document.getElementById("Game");
+const ctx = canvas.getContext("2d");
+ctx.fillStyle = "red";
+ctx.fillRect(0, 0, 1, 3);
